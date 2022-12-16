d77e5de9f2107564596ee3f6871f1f81238c2236
WangDanmeng <WangDanmeng98@163.com>
2022-12-16 21:55:33 +0800

    webpack插件htmlWebpackPlugin生成html文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -7,4 +8,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack demo 2',
+      template: 'src/assets/index.html',
+    }),
+  ],
 }
